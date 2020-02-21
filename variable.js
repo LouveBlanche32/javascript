7c31413fa55c79b0acda634a57fe3b0469ba8b32
LouveBlanche32 <bidouilh.s@yahoo.com>
2020-02-21 12:00:01 +0100

    message de commit

---
+++ variable.js
@@ -0,0 +1,42 @@
+let age=52;
+console.log(age)
+let année=1983;
+console.log(année)
+let somme= age + année;
+console.log(somme)
+let prenom= 'sandrine';
+let nom= 'bidouilh';
+let prenomEtnom= prenom+" "+nom;
+console.log(prenomEtnom)
+
+
+switch (1.1){
+	case'sarah':
+		alert('sarah Connor?')
+		break;
+
+	case'Siri':
+		alert('Je n'ai pas compris votre question?')
+		break;
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
